c311592c5ee692ac757b95a22dfe86b463b04da8
U-CURRIC\09964387 <09964387@P214-09964387.curric.parkville-co.wan>
2022-09-04 15:40:55 +1000

    first commit

---
+++ Task1.js
@@ -0,0 +1,42 @@
+
+//Vue.component('component1', {
+//    template: <div>This is component 1</div>
+//    data: function () {
+//        return {
+//            message: 'This is component 1',
+//        };
+//    },
+//});
+//Vue.component('component2', {
+//    template: <div>This is component 2</div>
+//});
+
+Vue.component('global',{
+    template: '<div>This is global<div>',
+});
+
+var component1 = {
+    template: '<div><global></global>This is component 1<div>',
+};
+
+var component2 = {
+    template: '<div><component1></component1>This is component 2<div>',
+    components: {
+        component1,
+    },
+};
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        text: 'hello vue',
+    },
+    components:{
+        component1,
+        component2,
+    },
+});
+
+
+// The activity was a great example into differentiating global and local registers.
+// The content lead to the creation of a Vue application
